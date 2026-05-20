9ce9ab649275a8bb9dc0ede935e8c5758ada2a7d
Sapier <lay22242@gmail.com>
2026-05-20 15:57:55 +0900

    1. Cutter resolves a broken state at startup -> Add random number seeds. 2. Error with tick too fast -> Separation with SDL_GetTicks()-based timers.

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,15 @@
 #define GL_SILENCE_DEPRECATION 
 #endif
 #include <SDL_opengl.h>
+#include <ctime>
 
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
-#include "Cutter.h"
+#include "Backend/Cutter.h"
 #include "MachineController.h"
+#include "UI/MachineCard.h"
 
 int main(int argc, char* argv[]) {
     // Initialize SDL
@@ -59,10 +61,16 @@ int main(int argc, char* argv[]) {
  /* End of CROSS-PLATFORM */
 
     Cutter cutter;
+    //cutter.acceptItem();
     MachineController ctrlCutter(&cutter);
     int tick = 0;
-
     bool running = true;
+
+    srand((unsigned int)time(nullptr));
+
+    Uint32 lastTickTime = SDL_GetTicks();
+    const Uint32 TICK_INTERVAL = 1000; // 1초마다 1틱
+
     while (running)
     {
         // Handle events
@@ -73,18 +81,24 @@ int main(int argc, char* argv[]) {
             if (event.type == SDL_QUIT) running = false;
         }
 
+        Uint32 now = SDL_GetTicks();
+        if (now - lastTickTime >= TICK_INTERVAL) {
+            if (cutter.hasOutputReady()) cutter.collectOutput();
+            if (cutter.getState() == MachineState::IDLE) cutter.acceptItem();
+            cutter.update(tick++);
+            lastTickTime = now;
+        }
+
         // Start ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
-        
-        cutter.update(tick++);
+
         MachineSnap snap = ctrlCutter.getSnapshot();
 
         // Your UI goes here
-        ImGui::Begin("Hello ImGui!");
-        ImGui::Text("Welcome to Dear ImGui!");
-        ImGui::End();
+
+        renderMachineCard(ctrlCutter);
         
         // Render
         ImGui::Render();
