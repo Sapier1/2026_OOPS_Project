a44f0010047a09d629145dd5530e3b293835ff8d
Sapier <lay22242@gmail.com>
2026-06-11 13:53:06 +0900

    Refactoring : Add breakdownProb in MachineSnap.h to show breakdownprob in machine inspector.

--- src/MachineController.cpp
+++ src/MachineController.cpp
@@ -2,10 +2,10 @@
 
 MachineController::MachineController(
     AbstractMachine* machine, 
-    const Conveyor* inputConveyor
-)
+    const Conveyor* inputConveyor)
 : m_machine(machine),
-  m_inputConveyor(inputConveyor) {}
+  m_inputConveyor(inputConveyor)
+{}
 
 void MachineController::onRepairClicked() {
     if (m_machine) m_machine->repair();
@@ -17,15 +17,24 @@ void MachineController::onForceBreakClicked() {
 
 MachineSnap MachineController::getSnapshot() const {
     if (!m_machine) return { "Unknown", 0, 0.0f };
+
+    int queueSize = m_inputConveyor
+        ? m_inputConveyor->getSize()
+        : (m_machine->hasItem() ? 1 : 0);
+ 
+    int queueCapacity = m_inputConveyor
+        ? m_inputConveyor->getCapacity()
+        : 1;
+
     return {
         m_machine->getMachineName(),
         (int)m_machine->getState(),
         m_machine->getProgress(),
-        m_inputConveyor ? m_inputConveyor->getSize()
-            : (m_machine->hasItem() ? 1 : 0),
-        5, // queueCapacity (기본값)
+        queueSize,
+        queueCapacity,
         m_machine->getCompletedCount(),
         m_machine->getHealth(),
-        m_machine->getProcessTime()
+        m_machine->getProcessTime(),
+        m_machine->getBreakdownProb()
     };
 }
